65980cafcb9b41a922e0073478a3a0d42b7c56fd
orbartal <Or.Bartal@intelliware.ca>
2017-10-16 09:36:20 -0400

    Add router and hello world page in it.

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { HttpModule } from '@angular/http';
+//import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppHello } from './components/1/hello/hello.component';
 import { AppComponent }  from './app.component';
 
+
+//Will be used to map url to pages\component in src\app\app.component.html
+const appRoutes: Routes = [
+   { path: '1Hello', component: AppHello },
+];
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes)  ], //, HttpModule
+  declarations: [ AppComponent, AppHello  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ angular2/src/app/components/1/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component ({
+   selector: 'my-hello',
+   template: '<h1>hello</h1><p>Hello from in text template</p>',
+})
+export class AppHello  {
+}
